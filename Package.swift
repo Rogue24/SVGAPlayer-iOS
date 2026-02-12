eca4496098777deed64d96ab0b3f84af2e78774e
Rogue.Ping <zhoujianping24@hotmail.com>
2026-02-12 20:38:15 +0800

    兼容SPM；内置Protobuf；移除多余文件.

--- Package.swift
+++ Package.swift
@@ -15,38 +15,39 @@ let package = Package(
         )
     ],
     dependencies: [
-        // Protobuf（pbobjc runtime 在这里）
-        .package(
-            url: "https://github.com/apple/swift-protobuf.git",
-            exact: "1.29.0" // 对应 Protobuf 3.29.x
-        ),
-
-        // SSZipArchive
+        // ZipArchive（SSZipArchive）
         .package(
             url: "https://github.com/ZipArchive/ZipArchive.git",
-            from: "2.6.0"
-        )
+//            exact: "2.4.3" // 指定 2.4.3
+            from: "2.0.0" // >= 2.0.0 且 < 3.0.0
+        ),
     ],
     targets: [
+        // Protobuf ObjC runtime
         .target(
-            name: "SVGAPlayer",
-            path: "Source",
+            name: "Protobuf",
+            path: "Protobuf",
             publicHeadersPath: ".",
             cSettings: [
-                // 等价 podspec 里的：
-                // GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS=1
-                .define("GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS", to: "1"),
-
-                // pbobjc 头文件搜索
-                .headerSearchPath("pbobjc")
+                .unsafeFlags(["-fno-objc-arc"]), // 关闭 ARC
+                .headerSearchPath("protobuf"),
+            ]
+        ),
+        // 主库
+        .target(
+            name: "SVGAPlayer",
+            dependencies: [
+                "Protobuf",
+                .product(
+                    name: "ZipArchive",
+                    package: "ZipArchive"
+                ),
             ],
+            path: "Source",
+            publicHeadersPath: ".",
             linkerSettings: [
                 .linkedFramework("AVFoundation"),
                 .linkedLibrary("z")
-            ],
-            dependencies: [
-                .product(name: "SSZipArchive", package: "ZipArchive"),
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
             ]
         )
     ]
